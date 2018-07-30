0de3941af9d58469f534cfe0967bd84530e941a7
Demo User <demouser@MacBook-Pro.local>
2018-07-30 11:35:07 -0700

    Sign in with Google button, added to main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import webapp2
+import os
+import random
+import jinja2
+
+
+jinja_current_directory = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+
+class HelloHandler(webapp2.RequestHandler):
+    def get(self):
+        welcome_template = jinja_current_directory.get_template('/mainpage.html')
+        self.response.write(welcome_template.render())
+
+
+app = webapp2.WSGIApplication([
+    ('/', HelloHandler)
+], debug=True)

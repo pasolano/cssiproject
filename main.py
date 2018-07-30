87cc4fd48acc0fc67bcaef0438038c931db6912b
Demo User <demouser@MacBook-Pro.local>
2018-07-30 15:06:23 -0700

    main.py edits

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import webapp2
 import os
 import random
 import jinja2
-
+from google.appengine.api import users
 
 
 jinja_current_directory = jinja2.Environment(
@@ -11,6 +11,15 @@ jinja_current_directory = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+user = users.get_current_user()
+if user:
+    nickname = user.nickname()
+    logout_url = users.create_logout_url('/')
+    greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
+        nickname, logout_url)
+else:
+    login_url = users.create_login_url('/')
+    greeting = '<a href="{}">Sign in</a>'.format(login_url)
 
 
 class HelloHandler(webapp2.RequestHandler):

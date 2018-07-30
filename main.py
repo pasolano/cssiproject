86e95c984307e595d15ebd35f4323c019f581477
Demo User <demouser@MacBook-Pro.local>
2018-07-30 12:15:29 -0700

    fixed error

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import jinja2
 
 
 jinja_current_directory = jinja2.Environment(
-
-JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
